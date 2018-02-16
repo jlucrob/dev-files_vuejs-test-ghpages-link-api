6373c7a894c88be1bc0a5b89f6a0899b148476a2
JLROB11 <jlucrob@hotmail.com>
2018-02-16 18:57:21 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dev-files_vuejs-test-ghpages-link-api
 Dev Files for vuejs-test-ghpages-link-api
+
+Find the built files here: https://github.com/jlucrob/vuejs-test-ghpages-link-api
+
+Find the live app here: https://jlucrob.github.io/vuejs-test-ghpages-link-api
---
+++ src/components/ShowData.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="show-data">
+    <h1>Usernames from test-rest-api on Heroku</h1>
+    <div>
+      <li v-for="item in data">{{item.username}}</li>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'show-data',
+  data () {
+    return {
+      data: []
+    }
+  },
+  created(){
+    axios.get('https://enigmatic-atoll-46436.herokuapp.com/users')
+    .then(result => {
+      console.log(result.data);
+      this.data = result.data;
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 10px 10px;
+  list-style: none;
+}
+a {
+  color: #42b983;
+}
+</style>
